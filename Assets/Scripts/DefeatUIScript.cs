0a00634a7607d8dfa58c0846397d57f39a1dffb6
orzuri <tzori.or@gmail.com>
2017-09-19 23:16:20 +0300

    Adding Enemy

---
+++ Assets/Scripts/DefeatUIScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DefeatUIScript : MonoBehaviour {
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    public void onClick()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+}
---
+++ Assets/Scripts/enemyAI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemyAI : MonoBehaviour {
+    public Transform player;
+    public float playerDistance;
+    public float rotationDamping;
+    public float moveSpeed;
+    public GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerDistance = Vector3.Distance(player.position, transform.position);
+
+        if(playerDistance < 15f)
+        {
+            lookAtPlayer();
+        }
+
+        if(playerDistance < 12f)
+        {
+            chase();
+        }
+	}
+
+    private void chase()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+    }
+
+    private void lookAtPlayer()
+    {
+        Vector3 v = new Vector3(transform.position.x, player.position.y , transform.position.z);
+        Quaternion rotation = Quaternion.LookRotation(player.position - v);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        menu.SetActive(true);
+        GameObject.Find("FirstPersonCamera").SendMessage("GameOver");
+    }
+}
